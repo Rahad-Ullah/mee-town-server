2484d5a9425f18694f13f771cd6208e7b1fc1c61
Rahad-Ullah <rahadullah10@gmail.com>
2025-07-26 11:13:37 +0600

    Added socket on creating message for live chat

--- src/app/modules/message/message.service.ts
+++ src/app/modules/message/message.service.ts
@@ -13,11 +13,22 @@ export const createMessage = async (payload: IMessage): Promise<IMessage> => {
     isDeleted: false,
     participants: { $in: [payload.sender] },
   });
-  if (!isChatExist) {
-    throw new Error('Chat not found or deleted');
-  }
+  if (!isChatExist) throw new Error('Chat not found or deleted');
+  if (!payload.text && !payload.image)
+    throw new Error('Text or image is required');
 
   const result = await Message.create(payload);
+
+  // emit socket event for new message
+  //@ts-ignore
+  const io = global.io;
+  if (io) {
+    io.emit(`getMessage::${payload.chat}`, result);
+  }
+
+  // update the chat to sort it to the top
+  await Chat.findByIdAndUpdate(payload.chat, {});
+
   return result;
 };
 
