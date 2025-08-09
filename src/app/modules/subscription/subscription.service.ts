3ad4e17a094199f606996cebdf736c84255fa186
Rahad-Ullah <rahadullah10@gmail.com>
2025-08-09 13:22:18 +0600

    Added creating subscription api

--- src/app/modules/subscription/subscription.service.ts
+++ src/app/modules/subscription/subscription.service.ts
@@ -1,7 +1,21 @@
+import { Package } from '../package/package.model';
+import { User } from '../user/user.model';
 import { ISubscription } from './subscription.interface';
+import { Subscription } from './subscription.model';
 
 // create subscription schema
 const createSubscriptionIntoDB = async (payload: ISubscription) => {
+  // check if the package is exists
+  const existingPackage = await Package.findById(payload.package);
+  if (!existingPackage) {
+    throw new Error('Package not found');
+  }
+
+  // push the package price to the subscription
+  payload.amount = existingPackage.price;
+
+  //! ---- todo: verify transaction data from apple api ----
+
   //   const response = await appleApi.getTransactionInfo(payload.transactionId);
 
   // Decode the signed JWS payload to get the transaction data
@@ -9,9 +23,17 @@ const createSubscriptionIntoDB = async (payload: ISubscription) => {
   //     response.signedTransactionInfo
   //   );
 
-  // ---- todo: after verify transaction data from apple api then save it into db and update the user subscription
+  //? ---- todo: after verify transaction data from apple api then save it into db and update the user subscription
 
-  return payload;
+  // create subscription
+  const result = await Subscription.create(payload);
+
+  // update the user subscription
+  await User.findByIdAndUpdate(payload.user, {
+    subscription: result._id,
+  });
+
+  return result;
 };
 
 // update subscription
