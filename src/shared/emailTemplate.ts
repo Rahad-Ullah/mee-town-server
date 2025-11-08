6ed38d7138bb6ad9b65142740b8c70f9f5ceba2a
Rahad-Ullah <rahadullah10@gmail.com>
2025-11-08 18:30:41 +0600

    refact: email templates

--- src/shared/emailTemplate.ts
+++ src/shared/emailTemplate.ts
@@ -5,21 +5,23 @@ const createAccount = (values: ICreateAccount) => {
   const data = {
     to: values.email,
     subject: 'Verify your account',
-    html: `<body style="font-family: Trebuchet MS, sans-serif; background-color: #f9f9f9; margin: 50px; padding: 20px; color: #555;">
-    <div style="width: 100%; max-width: 600px; margin: 0 auto; padding: 20px; background-color: #fff; border-radius: 10px; box-shadow: 0 0 10px rgba(0,0,0,0.1);">
-        <img src="http://72.167.224.54:5000/public/logo.png" alt="Logo" style="display: block; margin: 0 auto 20px; width:150px" />
-          <h2 style="color: #002C66; font-size: 24px; margin-bottom: 20px; text-align: center">Hey! ${
-            values.name ? values.name + ',' : ''
-          } Your ${config.server_name} Account Credentials</h2>
-        <div style="text-align: center;">
-            <p style="color: #555; font-size: 16px; line-height: 1.5; margin-bottom: 20px;">Your single use code is:</p>
-            <div style="background-color: #002C66; width: 80px; padding: 10px; text-align: center; border-radius: 8px; color: #fff; font-size: 25px; letter-spacing: 2px; margin: 20px auto;">${
-              values.otp
-            }</div>
-            <p style="color: #555; font-size: 16px; line-height: 1.5; margin-bottom: 20px;">This code is valid for 3 minutes.</p>
-        </div>
-    </div>
-</body>`,
+    html: `
+      <body style="font-family: Trebuchet MS, sans-serif; background-color: #f9f9f9; margin: 50px; padding: 20px; color: #555;">
+          <div style="width: 100%; max-width: 600px; margin: 0 auto; padding: 20px; background-color: #fff; border-radius: 10px; box-shadow: 0 0 10px rgba(0,0,0,0.1);">
+              <img src="http://72.167.224.54:5000/public/logo.png" alt="Logo" style="display: block; margin: 0 auto 20px; width:150px" />
+                <h2 style="color: #002C66; font-size: 24px; margin-bottom: 20px; text-align: center">Hey! ${
+                  values.name ? values.name + ',' : ''
+                } Your ${config.server_name} Account Credentials</h2>
+              <div style="text-align: center;">
+                  <p style="color: #555; font-size: 16px; line-height: 1.5; margin-bottom: 20px;">Your single use code is:</p>
+                  <span style="background-color: #002C66; padding: 10px; text-align: center; border-radius: 8px; color: #fff; font-size: 25px; letter-spacing: 2px; margin: 20px auto;">${
+                    values.otp
+                  }</span>
+                  <p style="color: #555; font-size: 16px; line-height: 1.5; margin-bottom: 20px;">This code is valid for 3 minutes.</p>
+              </div>
+          </div>
+      </body>
+    `,
   };
   return data;
 };
@@ -28,17 +30,19 @@ const resetPassword = (values: IResetPassword) => {
   const data = {
     to: values.email,
     subject: 'Reset your password',
-    html: `<body style="font-family: Trebuchet MS, sans-serif; background-color: #f9f9f9; margin: 50px; padding: 20px; color: #555;">
-    <div style="width: 100%; max-width: 600px; margin: 0 auto; padding: 20px; background-color: #fff; border-radius: 10px; box-shadow: 0 0 10px rgba(0,0,0,0.1);">
-        <img src="http://72.167.224.54:5000/public/logo.png" alt="Logo" style="display: block; margin: 0 auto 20px; width:150px" />
-        <div style="text-align: center;">
-            <p style="color: #555; font-size: 16px; line-height: 1.5; margin-bottom: 20px;">Your single use code is:</p>
-            <div style="background-color: #002C66; width: 80px; padding: 10px; text-align: center; border-radius: 8px; color: #fff; font-size: 25px; letter-spacing: 2px; margin: 20px auto;">${values.otp}</div>
-            <p style="color: #555; font-size: 16px; line-height: 1.5; margin-bottom: 20px;">This code is valid for 3 minutes.</p>
-                <p style="color: #b9b4b4; font-size: 16px; line-height: 1.5; margin-bottom: 20px;text-align:left">If you didn't request this code, you can safely ignore this email. Someone else might have typed your email address by mistake.</p>
+    html: `
+      <body style="font-family: Trebuchet MS, sans-serif; background-color: #f9f9f9; margin: 50px; padding: 20px; color: #555;">
+        <div style="width: 100%; max-width: 600px; margin: 0 auto; padding: 20px; background-color: #fff; border-radius: 10px; box-shadow: 0 0 10px rgba(0,0,0,0.1);">
+            <img src="http://72.167.224.54:5000/public/logo.png" alt="Logo" style="display: block; margin: 0 auto 20px; width:150px" />
+            <div style="text-align: center;">
+                <p style="color: #555; font-size: 16px; line-height: 1.5; margin-bottom: 20px;">Your single use code is:</p>
+                <span style="background-color: #002C66; width: 80px; padding: 10px; text-align: center; border-radius: 8px; color: #fff; font-size: 25px; letter-spacing: 2px; margin: 20px auto;">${values.otp}</span>
+                <p style="color: #555; font-size: 16px; line-height: 1.5; margin-bottom: 20px;">This code is valid for 3 minutes.</p>
+                    <p style="color: #b9b4b4; font-size: 16px; line-height: 1.5; margin-bottom: 20px;text-align:center">If you didn't request this code, you can safely ignore this email. Someone else might have typed your email address by mistake.</p>
+            </div>
         </div>
-    </div>
-</body>`,
+      </body>
+    `,
   };
   return data;
 };
